efe24a8ca983c0b1154cb08c5673dc6e3f8bcad0
unknown <Robindiegoa@gmail.com>
2021-03-26 12:20:38 -0300

    Versao 1.0

---
+++ JS/main.js
@@ -0,0 +1,15 @@
+function redirecionarFb(){
+    window.open("https://www.facebook.com/robsondiego.andrade")
+}
+
+function redirecionarIg(){
+    window.open("https://www.instagram.com/therealrobs_/")
+}
+
+function redirecionarTt(){
+    window.open("https://twitter.com/TheRealRobs__")
+}
+
+function redirecionarIn(){
+    window.open("https://www.linkedin.com/in/robson-diego-andrade-de-oliveira-071b53204/")
+}
